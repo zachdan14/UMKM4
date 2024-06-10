b2f70b1f5e95aeb06bbd784abcd874eb8bacd654
lukypasya <lukyyoshinta503@gmail.com>
2024-06-10 08:46:27 +0700

    menghilangkan "layanan kami"

--- resources/views/user/layout/navbar.blade.php
+++ resources/views/user/layout/navbar.blade.php
@@ -1,24 +1,24 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>homepage</title>
 
-    <!-- Css Link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    Css Link -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.bootstrap5.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.0/css/responsive.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.0/css/responsive.bootstrap5.css"> -->
         <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> -->
     <!-- End -->
     <!-- Bootsrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> -->
     <!-- End -->
     <!-- JS -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
@@ -32,12 +32,12 @@
                 responsive: true
             });
         });
-    </script>
+    </script> -->
     <!-- End -->
-</head>
-<body>  
+ <!-- </head>
+<body>   -->
    
-<nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
+<!-- <nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
     <div class="container">
       <a class="navbar-brand" href="#">
       <img
@@ -64,15 +64,15 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#!"><i class="fa fa-home" aria-hidden="true"></i>Home</a>
-        </li>
-        <li class="nav-item">
+        </li> 
+        < <li class="nav-item">
           <a class="nav-link" href="#!"><i class="fas fa-bell pe-2"></i>Layanan kami</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#!"><i class="fa fa-commenting" aria-hidden="true"></i>medsos</a>
-        </li>
-        </li>
-        <a href="#" class="nav-item user-icon"><i class="fas fa-user"></i></a> <!-- Ikon User -->
+        </li> -->
+        <!-- </li>
+        <a href="#" class="nav-item user-icon"><i class="fas fa-user"></i></a> Ikon User
       </ul>
       </div>
       </div>
@@ -95,4 +95,4 @@
       @yield('content')
     </div>
 </body>
-</html>
+</html> --> 
